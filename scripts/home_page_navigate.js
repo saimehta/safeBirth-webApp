75f04b58f942b7bc73ef62086a5d9e71057a6e88
Colin McNabb <mcnabb.colin.lee@gmail.com>
2021-06-03 18:17:05 -0600

    Clean up directories, add scripts

---
+++ scripts/home_page_navigate.js
@@ -0,0 +1,27 @@
+function homePageNavigate(buttonId) {
+    switch (buttonId) {
+        case 'register':
+            window.location.href='register.html';
+            break;
+
+        case 'login':
+            window.location.href='login.html';
+            break;
+
+        case 'forgot_id':
+            window.location.href='forgot_id.html';
+            break;
+        
+        case 'forgot_password':
+            window.location.href='forgot_password.html'
+            break;
+    }
+}
+
+const buttons = document.querySelectorAll('button')
+buttons.forEach((button) => {
+    console.log(button.id)
+    button.addEventListener('click', () => {
+        homePageNavigate(button.id);
+    });
+});
