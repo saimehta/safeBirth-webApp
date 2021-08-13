6706765554d78dcdbc85788d3bd5e602d38e3a9d
Colin McNabb <mcnabb.colin.lee@gmail.com>
2021-08-13 16:05:32 +0200

    Change file names for script and style files

---
+++ script.js
@@ -0,0 +1,26 @@
+const homeButtons = document.querySelectorAll(".homepage_button");
+function homePageNavigate(buttonId) {
+  switch (buttonId) {
+    case "register":
+      window.location.href = "register.html";
+      break;
+
+    case "login":
+      window.location.href = "login.html";
+      break;
+
+    case "forgot_id":
+      window.location.href = "forgot_id.html";
+      break;
+
+    case "forgot_password":
+      window.location.href = "forgot_password.html";
+      break;
+  }
+}
+
+homeButtons.forEach(button) => 
+  button.addEventListener("click", () => {
+    homePageNavigate(button.id);
+  );
+});
