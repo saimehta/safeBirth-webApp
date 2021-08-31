300c9c1f67f04a603cbbc66afce4c99c4b335435
Colin McNabb <mcnabb.colin.lee@gmail.com>
2021-08-31 14:37:07 -0600

    Final tweaks almost done

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
+// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
+// import * as firebase from "firebase/app"
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
 // constants
+const firebaseConfig = {
+  apiKey: "AIzaSyBBSufFogAcMd_Mo1GlzUTeb9i44Yzr16M",
+  authDomain: "safebirth-test.firebaseapp.com",
+  projectId: "safebirth-test",
+  storageBucket: "safebirth-test.appspot.com",
+  messagingSenderId: "701634915042",
+  appId: "1:701634915042:web:5465a5c7dbd5cdaf3a4cbb",
+  measurementId: "G-FDN29VVEJT"
+}
+firebase.initializeApp(firebaseConfig);
+
 const homeButtons = document.querySelectorAll(".homepage_button");
 const navButtons = document.querySelectorAll(".navigation_button");
 const patientSubmit = document.querySelectorAll(".patient_submit");
